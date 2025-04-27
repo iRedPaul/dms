e0ff61b5be3eaf375ea921f48a761aab07b21ae8
Paul <66887666+iRedPaul@users.noreply.github.com>
2025-04-27 22:01:49 +0200

    Create Document.js

---
+++ backend/models/Document.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const DocumentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  path: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: Number,
+    required: true
+  },
+  uploadedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Document', DocumentSchema);
