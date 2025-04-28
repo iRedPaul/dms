65ffc9fbb6ff264c91910703a6ac19ec89766765
Paul <66887666+iRedPaul@users.noreply.github.com>
2025-04-28 22:37:31 +0200

    Create Mailbox.js

---
+++ backend/models/Mailbox.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const MailboxSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    default: ''
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Mailbox', MailboxSchema);
