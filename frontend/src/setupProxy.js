4e18824e269ba3702f32278fe8e31ea4c65aaec0
Paul <66887666+iRedPaul@users.noreply.github.com>
2025-04-28 22:14:36 +0200

    Create setupProxy.js

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,19 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_URL || 'http://localhost:4000',
+      changeOrigin: true,
+    })
+  );
+  
+  app.use(
+    '/uploads',
+    createProxyMiddleware({
+      target: process.env.REACT_APP_API_URL || 'http://localhost:4000',
+      changeOrigin: true,
+    })
+  );
+};
