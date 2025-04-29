c131c2e8ee6af1305336552078b191ec810b41f8
Paul <66887666+iRedPaul@users.noreply.github.com>
2025-04-29 20:06:59 +0200

    Update setupProxy.js

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,22 +1,25 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+  // Konfiguration mit korrekter Domain
+  const API_HOST = process.env.REACT_APP_API_HOST || 'https://dms.home-lan.cc';
+  
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_API_URL || 'https://dms.home-lan.cc',
+      target: API_HOST,
       changeOrigin: true,
-      pathRewrite: {
-        '^/api': '', // Entferne '/api' vom Pfad für die Weiterleitung
-      },
+      secure: false, // Bei selbstsignierten Zertifikaten notwendig
+      pathRewrite: path => path // keine Umschreibung, damit die API-Pfade beibehalten werden
     })
   );
   
   app.use(
     '/uploads',
     createProxyMiddleware({
-      target: process.env.REACT_APP_API_URL || 'https://dms.home-lan.cc',
+      target: API_HOST,
       changeOrigin: true,
+      secure: false // Bei selbstsignierten Zertifikaten notwendig
     })
   );
 };
